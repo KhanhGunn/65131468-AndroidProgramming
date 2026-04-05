1192aa0fa57c7ca09b6e770291da19bd5242b572
KhanhGunn <khanh.nqu061205@gmail.com>
2026-04-05 23:42:41 +0700

    Xong bài thực hành 13

---
+++ BaiTH13ThayDoiFragment/app/src/main/java/gun/edu/baith13thaydoifragment/Fragment1.java
@@ -0,0 +1,30 @@
+package gun.edu.baith13thaydoifragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment1 extends Fragment {
+
+
+    public Fragment1() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+
+        }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+}
