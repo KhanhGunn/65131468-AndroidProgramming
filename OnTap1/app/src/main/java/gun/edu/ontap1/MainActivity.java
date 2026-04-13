925e02df072c8c99cb2ac91e6051597ac6948c75
KhanhGunn <khanh.nqu061205@gmail.com>
2026-04-13 09:54:19 +0700

    Ôn tập Xong Hàm xử lý sự kiện Onclick và bộ lắng nghe

--- OnTap1/app/src/main/java/gun/edu/ontap1/MainActivity.java
+++ OnTap1/app/src/main/java/gun/edu/ontap1/MainActivity.java
@@ -1,6 +1,8 @@
 package gun.edu.ontap1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,4 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+    public void XuLyCong(View view){
+        EditText edtA = findViewById(R.id.edtA);
+        EditText edtB = findViewById(R.id.edtB);
+        EditText edtKetQua = findViewById(R.id.edtKQ);
+
+        String StrA = edtA.getText().toString();
+        String StrB = edtB.getText().toString();
+
+        int so_A = Integer.parseInt(StrA);
+        int so_B = Integer.parseInt(StrB);
+
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+        edtKetQua.setText(strTong);
+    }
+
 }
